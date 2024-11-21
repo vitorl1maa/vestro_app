e55565a8e827998ef40c0adbdd7349ca1560104b
Vitor Lima Nascimento <vitor@inspell.com.br>
2024-11-21 13:27:37 -0300

    feat: config font app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Login from "./src/screens/Login";
+import { StyleSheet, View } from "react-native";
+import Login from "./src/screens/Login/Login";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/ui/theme";
+import {
+  useFonts,
+  OpenSans_300Light,
+  OpenSans_400Regular,
+  OpenSans_600SemiBold,
+  OpenSans_800ExtraBold,
+} from "@expo-google-fonts/open-sans";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_300Light,
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Login />
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Login />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#111111",
     alignItems: "center",
     justifyContent: "center",
   },
